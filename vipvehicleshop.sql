abe6dd726e27797db6fcb76bebbd22e0a52ed409
Miguel Ignacio <76574441+DLMarcos@users.noreply.github.com>
2021-05-30 13:09:15 -0400

    Update vipvehicleshop.sql

--- vipvehicleshop.sql
+++ vipvehicleshop.sql
@@ -1,18 +1,19 @@
 ALTER TABLE `users`
 	ADD `vip` TINYINT(1) NULL DEFAULT '0'
 ;
-----------------------------------------------
+
+SET NAMES UTF8MB4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `vehiclevip_categories`;
-CREATE TABLE `vehicle_categories`  (
+CREATE TABLE `vehiclevip_categories`  (
   `name` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `label` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   PRIMARY KEY (`name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+);
 
 INSERT INTO `vehiclevip_categories` VALUES ('vip', 'VIP');
--------------------------------------------------------------------
+
 DROP TABLE IF EXISTS `vipvehicles`;
 CREATE TABLE `vipvehicles`  (
   `name` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
@@ -20,8 +21,8 @@ CREATE TABLE `vipvehicles`  (
   `price` int(11) NOT NULL,
   `category` varchar(60) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`model`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+);
 
 INSERT INTO `vipvehicles` VALUES ('ZentornoVIP', 'police3', 1000000, 'vip');
 
-SET FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = (1)
